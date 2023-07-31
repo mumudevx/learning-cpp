3e3e681721c221a8fd25180b4d724cd7fcd174ac
Mumu <mumudevx@gmail.com>
2023-07-31 06:31:49 +0300

    structures and classes were added

---
+++ 05-structures-and-classes/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <utility>
+
+using namespace std;
+
+// There is no function definitions in structs
+struct Flower {
+    string name;
+    string family;
+    string siteType;
+};
+
+// There is available to define functions in class
+class Person {
+private:
+    string _fullName;
+    int _age;
+
+public:
+    explicit Person(string fullName, int age) {
+        _fullName = std::move(fullName);
+        _age = age;
+    }
+
+public:
+    void Introduce() {
+        std::cout << "Hello! " << "My name is " << this->_fullName << " I'm " << this->_age << " years old.";
+    }
+};
+
+int main() {
+    Flower dwarf = Flower{"Dwarf gentians", "Gentianaceae", "Wet meadows, bogs"};
+    std::cout << "Name: " << dwarf.name << std::endl;
+    std::cout << "Family: " << dwarf.family << std::endl;
+    std::cout << "Site Type: : " << dwarf.siteType << std::endl;
+
+    std::cout << "---------------------" << std::endl;
+
+    Person someOne = Person("Cristiana Owls", 24);
+    someOne.Introduce();
+
+    return 0;
+}
